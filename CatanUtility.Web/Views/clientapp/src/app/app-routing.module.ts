29e4bc8f24e7ad4671e8d673e1113b14f1d45b9c
Michael Dunn <michael.dunn.software@gmail.com>
2020-09-29 19:37:09 -0500

    Changed angular app to a routing app

--- CatanUtility.Web/Views/clientapp/src/app/app-routing.module.ts
+++ CatanUtility.Web/Views/clientapp/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule,
+  ]
 })
 export class AppRoutingModule { }
--- CatanUtility.Web/Views/clientapp/src/app/app.component.spec.ts
+++ CatanUtility.Web/Views/clientapp/src/app/app.component.spec.ts
@@ -19,4 +19,17 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it(`should have as title 'clientapp'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('clientapp');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('clientapp app is running!');
+  });
 });
